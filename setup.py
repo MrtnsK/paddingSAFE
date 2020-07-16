4e3de209bfb7c1d71bd935524ac978051eb7dd60
unknown <kemartin@student.42.fr>
2020-07-16 05:33:34 -0700

    deploy branch

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-
-setup(
-    name = "padding",
-    version = "0.1",
-    description = "Ce programme alligne correctement les informations de la journalisation",
-    executables = [Executable("padding.py")],
-)
